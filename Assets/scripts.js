f40bd426c06911541aa53646733e21441ee6e03d
Matthew <nyongezamatthew20@gmail.com>
2022-08-08 19:21:18 +0300

    INITIAL COMMIT

---
+++ Assets/scripts.js
@@ -0,0 +1,49 @@
+function openNav() {
+    document.getElementById("sidebar").style.marginRight = "0vw";
+  }
+  function closeNav() {
+    document.getElementById("sidebar").style.marginRight = "-70vw";
+  }
+  
+  
+  window.onscroll=function(){
+  var navbar = document.getElementById("nav");
+  console.log(navbar)
+    if(window.scrollY > 300){
+  
+      navbar.classList.add('bg-primary') 
+    }
+    else{
+      navbar.classList.remove("bg-primary");  
+  
+    }
+  }
+  
+  
+  
+  function nextSlide(url,ele){
+    var allimg = document.getElementsByClassName('slider-img')
+    for (let index = 0; index < allimg.length; index++) {
+      allimg[index].classList.remove('active-img')
+      
+    }
+    var bgimage = document.getElementById('home')
+    bgimage.style.backgroundImage = `url(${url})`;
+    ele.classList.add('active-img')
+  }
+  
+  function scrollRight(){
+    var reviewcontainer = document.getElementById("review-container");
+  
+    reviewcontainer.scrollBy(300, 0);
+  }
+  function scrollToLeft() {
+    var reviewcontainer = document.getElementById("review-container");
+    reviewcontainer.scrollBy(-100,0);
+  }
+
+
+
+ 
+
+  
